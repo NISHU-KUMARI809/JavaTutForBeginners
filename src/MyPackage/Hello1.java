71bcb0ec75482e15c198fe6014aecd5d8e22ee6b
Nishu satsangi <nk8686825@gmail.com>
2023-09-30 10:22:31 +0530

    Initial commit

---
+++ src/MyPackage/Hello1.java
@@ -0,0 +1,12 @@
+package MyPackage;
+public class Hello1
+{
+    public void fun()
+    {
+        System.out.println("This in Package MYPackage");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Heelo");
+    }
+}
